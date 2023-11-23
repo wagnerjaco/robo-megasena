380aad66ebaa62e3de3c4a2cda577e3474b898c8
wagnerjaco <wagnerjaco@gmail.com>
2023-11-23 13:02:16 -0300

    ok

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,7 +22,17 @@ namespace robo_megasena
                 numerodoconcurso = "2103";
             }
 
-            string url = "http://www1.caixa.gov.br/loterias/loterias/megasena/megasena_pesquisa_new.asp?submeteu=sim&opcao=concurso&txtConcurso=" + numerodoconcurso
+            string url = @"http://www1.caixa.gov.br/loterias/loterias/megasena/megasena_pesquisa_new.asp?submeteu=sim&opcao=concurso&txtConcurso=" + numerodoconcurso;
+            string html;
+
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers["cookie"] = "security =true";
+                html = wc.DownloadString(url);
+
+            }
+            Console.ReadKey();
+
 
            
 
